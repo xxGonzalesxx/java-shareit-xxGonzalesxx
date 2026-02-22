dd53dbfb45d3fde5c99b3aa52f38daacd1a0fde5
Stefan <stef.kir1999@gmail.com>
2026-02-22 22:44:27 +0400

    Add @ResponseStatus to exceptions

--- src/main/java/ru/practicum/shareit/exception/ForbiddenException.java
+++ src/main/java/ru/practicum/shareit/exception/ForbiddenException.java
@@ -1,5 +1,9 @@
 package ru.practicum.shareit.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class ForbiddenException extends RuntimeException {
     public ForbiddenException(String message) {
         super(message);
