732e523994acd65e17700842fb956fe6f4ebcd88
Stefan <stef.kir1999@gmail.com>
2026-03-03 22:34:48 +0400

    Удалён неиспользуемый импорт BookingMapper

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.item.mapper;
 
 import ru.practicum.shareit.booking.dto.BookingDto;
-import ru.practicum.shareit.booking.mapper.BookingMapper;
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Comment;
